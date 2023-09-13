aef8954615470751cabf4a76123ce633f2e970be
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-09-13 07:44:01 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
-function compareArrays(arr1, arr2) {
-  
+ function compareArrays(arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every(function(element, index) {
+		return element === arr2[index];
+	})
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender);
+	const ages = filteredUsers.map(user => user.age);
+	const avgAge = ages.reduce((sum, age) => sum + age, 0) / ages.length  || 0;
+	return avgAge;
 }
