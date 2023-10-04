e7b5735b0a5f97592d7171eaf18771bf064b2f7b
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-10-04 11:02:34 +0300

    Восьмое задание полностью готово after lesson

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,59 @@
+'use strickt'
 //Задача № 1
+//const md5 = require('js-md5'); // При подключении библиотеки для хеширования MD5 не работает тест
+
 function cachingDecoratorNew(func) {
-  
+	const cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(JSON.stringify(args));
+
+		const objectInCache = cache.find(item => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.result);
+			return "Из кеша: " + objectInCache.result;
+		}
+        
+        let result = func(...args);
+        const itemToAdd = {
+			hash,
+			result
+		};
+		cache.push(itemToAdd);
+
+		if (cache.length > 5) {
+			cache.shift();
+		}
+
+		return "Вычисляем: " + result;
+	}
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId;
   
-}
+    function wrapper(...args) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+  
+      if (!timeoutId) {
+        func.apply(this, args);
+        wrapper.count++;
+      }
+  
+      timeoutId = setTimeout(() => {
+        wrapper.count++;
+        func.apply(this, args);
+      }, delay);
+  
+      wrapper.allCount++;
+    }
+  
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+  
+    return wrapper;
+  }
